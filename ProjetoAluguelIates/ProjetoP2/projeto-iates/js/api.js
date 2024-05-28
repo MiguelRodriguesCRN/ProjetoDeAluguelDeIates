cd02b10292980966d9bd714c13219651fada0243
Miguel - Rodrigues <143560179+MiguelRodriguesCRN@users.noreply.github.com>
2024-05-27 21:06:34 -0300

    Upload de PastaReferente Ao Projeto

---
+++ ProjetoAluguelIates/ProjetoP2/projeto-iates/js/api.js
@@ -0,0 +1,77 @@
+function obterMensagens() {
+
+    var retorno = [];
+
+    var consulta = $.ajax({
+        url: 'https://app-uniesp-p2-b8d2992ac568.herokuapp.com/mensagens',
+        method: 'GET',
+        dataType: 'json',
+        async: false
+    }).fail(function(){
+        return retorno;
+    });
+
+    consulta.done(function(data) {
+        retorno = data;
+    });
+
+    return retorno;
+}
+
+function inserirMensagem(mensagem) {
+
+    /*
+
+    var mensagem = {
+            nome: "nome da pessoa", 
+            email: "email informado", 
+            mensagem: "a mensagem informada"} 
+
+    */
+
+    var inserir = $.ajax({
+
+        url: 'https://app-uniesp-p2-b8d2992ac568.herokuapp.com/mensagens',
+        method: 'POST',
+        data: JSON.stringify(mensagem),
+        dataType: 'json',
+        async: false,
+        contentType: 'application/json',
+    });
+}
+
+function validarUsuario(objLoginSenha) {
+
+    //email: admin@admin.com
+    //senha: '1234'
+
+    /*
+
+    var objLoginSenha = {
+            email: "email informado", 
+            senha: "senha informada"} 
+
+    */
+
+    var retorno = false;
+
+    var validacao = $.ajax({
+        url: 'https://app-uniesp-p2-b8d2992ac568.herokuapp.com/usuarios/validar',
+        method: 'POST',
+        dataType: 'json',
+        async: false,
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+                },
+        contentType: 'application/json',
+        data: JSON.stringify(objLoginSenha)
+    }).fail(function(){
+        return retorno;
+    });
+
+    validacao.done(function(data) {
+        retorno = data;
+    });
+
+    return retorno;
+}
